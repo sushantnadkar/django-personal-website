6866c50bec016d3bf51c79426daf1d9f1104362d
Sushant Nadkar <ssn.andymax@gmail.com>
2020-07-15 20:23:29 +0530

    Removed hard codded project listing Some css changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,6 @@ from django.views import generic
 from .models import Project
 
 class ProjectList(generic.ListView):
-	queryset = Project.objects.filter(status=1)
+	queryset = Project.objects.all().order_by("category").filter(status=1)
 	template_name = "portfolio.html"
-
+	context_object_name = "projects"
