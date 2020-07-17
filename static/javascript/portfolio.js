eac7a0805ddc60bf2fedd62bd8462187697a90b3
Sushant Nadkar <ssn.andymax@gmail.com>
2020-07-17 17:37:17 +0530

    Elements associated with custome js were prefixed with 'js-*' classes for easier identification Link to hide additional projects added Updated css for smooth scrolling

--- static/javascript/portfolio.js
+++ static/javascript/portfolio.js
@@ -4,29 +4,45 @@
     $('html').removeClass('no-js');
 
     // Scroll to top
-    $('#to-top').click(function() {
+    $('.js-to-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
     });
 
     // Scroll to first element
-    $('#lead-down span').click(function() {
+    $('.js-lead-down').click(function() {
         var scrollDistance = $('#lead').next().offset().top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
         }, 500);
     });
 
-    $('.navbar-collapse a').click(function(){
+    $('.js-scroll-to').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
 
     // Load additional projects
-    $('#view-more-projects').click(function(e) {
+    $('.js-view-more-projects').click(function(e) {
         e.preventDefault();
         $(this).fadeOut(300, function() {
             $('#more-projects').fadeIn(300);
+            $('.js-view-less-projects').fadeIn(300);
+        });
+    });
+
+    // Hide additional projects
+    $('.js-view-less-projects').click(function(e) {
+        e.preventDefault();
+        $(this).fadeIn(300, function() {
+            $('#more-projects').fadeOut(300);
+            $('.js-view-more-projects').fadeIn(300);
+            // adjust scroll after hidding #more-projects
+            // substract 100 so that ".js-view-more-projects" is not hidden behind navbar
+            let scrollDistance = $(".js-view-more-projects").offset().top - 100;
+            $('html, body').animate({
+                scrollTop: scrollDistance + 'px'
+            }, 500);
         });
     });
 
