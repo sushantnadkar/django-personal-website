641ef3bf1b9649b32b1f60c9ed12a2e1c9fbcdc4
Sushant Nadkar <ssn.andymax@gmail.com>
2020-09-17 22:28:54 +0530

    model issue added to issue tracker app

--- djangosite/issue_tracker/admin.py
+++ djangosite/issue_tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Issue
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+class IssueAdmin(SummernoteModelAdmin):
+    search_fields = ["title", "description"]
+    prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("description",)
+
+admin.site.register(Issue, IssueAdmin)
--- djangosite/issue_tracker/models.py
+++ djangosite/issue_tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Issue(models.Model):
+
+	STATUS = ((0, "New Issues"), (1, "Icebox"), (2, "Backlog"), (3, "In Progress"), (4, "Review/QA"), (5, "Closed"))
+
+	title = models.CharField(max_length=200, unique=True)
+	slug = models.SlugField(max_length=200, unique=True)
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	description = models.TextField()
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+	status = models.IntegerField(choices=STATUS, default=0)
+
+	class Meta:
+		ordering = ["-created_on"]
+
+	def __str__(self):
+		return self.title
